38eb7aaa4df1897d8addee8684f6f17c7d80c059
Jason Cartwright <jason@ensightenterprises.com>
2014-12-09 13:03:29 -0600

    Inquire & Settlement Status
    * implement inquire request/response
    * implement inquire_transaction method
    * implement settlement status request/response
    * implement settlement_status_transaction method

---
+++ spec/lib/cardconnect_sdk/settlement_status/request_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+module CardconnectSdk
+  module SettlementStatus
+    RSpec.describe Request, type: :lib do
+      context '.initialize' do
+        it 'instantiates the right class' do
+          expect(Request.new).to be_a(described_class)
+        end
+
+        it 'has the expected attributes' do
+          req = described_class.new(merchid: ENV['CARDCONNECT_MERCHANT_ID'], date: '1208')
+          expect(req.merchid).to eq(ENV['CARDCONNECT_MERCHANT_ID'])
+          expect(req.date).to eq('1208')
+        end
+      end
+
+      context '.to_json' do
+        it 'returns valid json' do
+          req = described_class.new(merchid: ENV['CARDCONNECT_MERCHANT_ID'], date: '1208')
+          json = req.to_json
+          expect(json).to eq(
+            '{"merchid":"496160873888","date":"1208"}'
+          )
+        end
+      end
+    end
+  end
+end
