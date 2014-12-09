a947ef772d12e8005ed3806dd885c8d0a7fc9688
Jason Cartwright <jason@ensightenterprises.com>
2014-12-09 11:03:00 -0600

    add missing void spec

---
+++ spec/lib/cardconnect_sdk/void/request_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module CardconnectSdk
+  module Void
+    RSpec.describe Request, type: :lib do
+      context '.initialize' do
+        it 'instantiates the right class' do
+          expect(Request.new).to be_a(described_class)
+        end
+
+        it 'has the expected attributes' do
+          req = create(:void_request)
+          expect(req.merchid).to eq(ENV['CARDCONNECT_MERCHANT_ID'])
+        end
+      end
+
+      context '.to_json' do
+        it 'returns valid json' do
+          req = create(:void_request, retref: '999999')
+          json = req.to_json
+          expect(json).to eq(
+            '{"merchid":"496160873888","retref":"999999","amount":"0.99"}'
+          )
+        end
+      end
+    end
+  end
+end
