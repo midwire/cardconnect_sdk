40279e3dbf3ddfa2ace986b15f90b505c996d2f3
Matt Segvich <msegvich@software-craftsmanship.com>
2015-05-05 11:09:06 -0500

    Supports status by merchant only, by date and by batch ID

--- lib/cardconnect_sdk/settlement_status/settlement_status.rb
+++ lib/cardconnect_sdk/settlement_status/settlement_status.rb
@@ -11,8 +11,18 @@ module CardconnectSdk
       "#{url}#{ENDPOINT}"
     end
 
-    def settlement_status_transaction(request)
-      response = RestClient.get "#{settlement_status_endpoint}?merchid=#{request.merchid}&date=#{request.date}", default_headers      
+    def settlement_status_for_batch(request)
+      response = RestClient.get "#{settlement_status_endpoint}?merchid=#{request.merchid}&batchid=#{request.batchid}", default_headers
+      Response.from_json(response.body)
+    end
+
+    def settlement_status_for_date(request)
+      response = RestClient.get "#{settlement_status_endpoint}?merchid=#{request.merchid}&date=#{request.date}", default_headers
+      Response.from_json(response.body)
+    end
+
+    def settlement_status_for_merchant(request)
+      response = RestClient.get "#{settlement_status_endpoint}?merchid=#{request.merchid}", default_headers
       Response.from_json(response.body)
     end
   end
